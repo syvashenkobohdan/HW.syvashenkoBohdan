cb1322685b64ea8b07dd8228021420bb3784b3ce
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-13 00:05:48 +0300

    Assignment #2. Block 1. задача 4

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -8,21 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      findDividers(number: 10)
+        findDividers(number: 6)
     }
     
     func findDividers(number: Int) {
         var counter = 0
         var dividers = 0
+        var ideal = 0
         var array: [Int] = []
         for _ in 0..<number {
             counter += 1
             if number % counter == 0 {
                 array.append(counter)
-                
+                if counter != number {
+                    ideal += counter
+                }
                 dividers += 1
                 
             }
@@ -30,9 +33,13 @@ class ViewController: UIViewController {
         }
         let string = array.map { String($0) }
             .joined(separator: ", ")
+        if number % ideal == 0 {
+            print("Number \(number) is ideal number and has \(dividers) dividers . They are \(string).")
+        }else {
+            print("Number \(number) is not ideal number and has \(dividers) dividers . They are \(string).")
+        }
         
-        print("Number \(number) has \(dividers) dividers . They are \(string)")
     }
-   
+    
 }
 
