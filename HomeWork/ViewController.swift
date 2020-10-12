dac11ad5aa8f2b002a905f16f6515bb706e40ca9
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-12 22:54:39 +0300

    Assignment. Цель UIView (Квадратная)

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -11,47 +11,47 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        makePyramid(amount: 10)
+        drawTarget(amount: -10)
     }
     
-    let spacing: CGFloat = 10
-    let boxSize: CGFloat = 50
-    var verticalLocation: CGFloat = 800
-    var horizontalLocation: CGFloat = 50
-    var startHorizontalLocation: CGFloat = 50
-    
-    
-    func drawBox(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat) {
+    func drawBox(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, color: UIColor, radius: CGFloat) {
         let box = UIView()
-        box.backgroundColor = UIColor.black
+        box.backgroundColor = color
         box.frame = CGRect(x: x, y: y, width: width, height: height)
         view.addSubview(box)
+//        Assignment 2 rounded Target
+//        box.layer.cornerRadius = radius
     }
     
-    func makeColumn(amount: Int) {
-        
-        for _ in 0..<amount {
-            drawBox(x: horizontalLocation, y: verticalLocation, width: boxSize, height: boxSize)
-            horizontalLocation += boxSize + spacing
-        }
-    }
-    
-    func makePyramid(amount: Int) {
-        var counter = amount
+    func drawTarget(amount: Int) {
         guard amount > 0 else {
             amountCheck()
             return
         }
-        for _ in 0..<amount {
-            
-            makeColumn(amount: counter)
-            counter -= 1
-            verticalLocation -= boxSize + spacing
-            startHorizontalLocation += (boxSize + spacing) / 2
-            horizontalLocation = startHorizontalLocation
-        }
+        let position: CGFloat = 200
+        let size: CGFloat = 400
+        var counter = 0
+        var color: UIColor = UIColor.red
+        var spacing: CGFloat = 0
+        var radius: CGFloat = 0
         
+        for _ in 0..<amount {
+            counter += 1
+            spacing += 10
+            radius = (size - (spacing * 2)) / 2
+            
+            if counter % 2 != 0 {
+                color = UIColor.red
+            }else {
+                color = UIColor.green
+            }
+            
+            
+            
+            drawBox(x: position + spacing, y: position + spacing, width: size - (spacing * 2), height: size - (spacing * 2), color: color, radius: radius)
+            
+            
+        }
     }
     
     func amountCheck() {
@@ -61,7 +61,7 @@ class ViewController: UIViewController {
         view.addSubview(textBox)
         
     }
-    
+   
 }
 
 
