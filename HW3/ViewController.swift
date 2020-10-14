f8da67fd506e1dbc5ded0348af84dcb2d6e553d0
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-14 18:03:32 +0300

    Assignment #2. Block 2. задача 3

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -11,30 +11,32 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       countMoney(grant: 700, moneyNeeded: 1000, percent: 3, studentYear: 10)
         
         
-      
+        countMonth(savings: 2400, moneyNeed: 1000, studentGrant: 700, percent: 3)
+        
     }
-    
-    func countMoney(grant: Double, moneyNeeded: Double, percent: Double, studentYear: Int) {
-        
-        var moneyNeeded = moneyNeeded
-        var money = 0.0
-        var moneyNotEnought = 0.0
-        
-        for i in 0..<studentYear {
-            if i > 0 {
-            moneyNeeded += moneyNeeded * (percent / 100)
+    func countMonth(savings: Double, moneyNeed: Double, studentGrant: Double, percent: Double){
+        var savings: Double = savings
+        var moneyNeeded: Double = moneyNeed
+        let studentGrant: Double = studentGrant
+        var monthStudentCanExist = 0
+        while savings > 0 {
+            
+            if monthStudentCanExist > 0 {
+                moneyNeeded += moneyNeeded * (percent / 100)
             }
-            money += moneyNeeded
+            savings -= (moneyNeeded - studentGrant)
+            monthStudentCanExist += 1
+            print(savings)
+            
             
             
         }
-        moneyNotEnought = money - (grant * Double(studentYear))
-        print("Всего студенту нужно \(String(format: "%.1f", money)) грн. в год . Учитывая стипендию на жизнь студенту в год не хватает \(String(format: "%.1f", moneyNotEnought)) грн.")
+        if savings < 0 {
+            monthStudentCanExist -= 1
+        }
+        print("Студент сможет прожить \(monthStudentCanExist) месяцев !")
     }
-   
-
 }
 
