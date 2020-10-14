1c2d87448b17324581925389c9e6c4b329e5ef04
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-14 18:07:41 +0300

    Assignment #2. Block 2. задача 4

--- HW3/ViewController.swift
+++ HW3/ViewController.swift
@@ -8,35 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        countMonth(savings: 2400, moneyNeed: 1000, studentGrant: 700, percent: 3)
-        
+       returnReversed(variable: 54)
     }
-    func countMonth(savings: Double, moneyNeed: Double, studentGrant: Double, percent: Double){
-        var savings: Double = savings
-        var moneyNeeded: Double = moneyNeed
-        let studentGrant: Double = studentGrant
-        var monthStudentCanExist = 0
-        while savings > 0 {
-            
-            if monthStudentCanExist > 0 {
-                moneyNeeded += moneyNeeded * (percent / 100)
-            }
-            savings -= (moneyNeeded - studentGrant)
-            monthStudentCanExist += 1
-            print(savings)
-            
-            
-            
-        }
-        if savings < 0 {
-            monthStudentCanExist -= 1
-        }
-        print("Студент сможет прожить \(monthStudentCanExist) месяцев !")
+    func returnReversed(variable: Int){
+        
+        let myStr: String = String(variable)
+        let reversedInt = Int(String(myStr.reversed())) ?? 0
+        print("Перевернув число \(variable) получим число \(reversedInt)")
     }
+
 }
 
