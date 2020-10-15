1151a1fb99c9c05bb57a722738081647b22b3940
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-15 10:07:39 +0300

    Initial Commit

---
+++ HW4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW4
+//
+//  Created by Богдан Сивашенко on 15.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
