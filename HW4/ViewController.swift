5d69b43e5d477db44a6cec7e8a1bafa181c527ae
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-15 10:13:29 +0300

    Assignment #3. задача 1

--- HW4/ViewController.swift
+++ HW4/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       print(countSymbols(name: "Bohdan"))
     }
-
+    func countSymbols(name: String) -> Int {
+        return name.count
+    }
 
 }
 
