3665289b5114bb68288e0640bd59487a327052f9
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-26 22:36:53 +0200

    Assignment #3. задача 6

--- HW4/ViewController.swift
+++ HW4/ViewController.swift
@@ -11,21 +11,61 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(calcStuff(number: "231231"))
+        print(passwordCheck(password: "asda1231"))
     }
     
-    func calcStuff(number: String) -> String {
-        var start = number
-        var counter = start.count
-        for _ in start {
-            counter -= 1
-            if counter % 3 == 0 , counter != 0 {
-                start.insert(",", at: start.index(start.endIndex, offsetBy: -counter))
+    func passwordCheck(password: String) -> String {
+        var score: Int = 5
+        var checks: [String: Bool] = ["isNumber": false, "isLetter": false, "isSymbol": false, "isUppercase": false, "isLowercase": false]
+        var returnString: String = ""
+        
+        for chr in password {
+            if chr.isNumber {
+                checks["isNumber"]  = true
+            } else if chr.isLetter, chr.isUppercase {
+                checks["isLetter"] = true
+                checks["isUppercase"] = true
+            } else if chr.isLetter, chr.isLowercase {
+                checks["isLetter"] = true
+                checks["isLowercase"] = true
+            } else if chr.isPunctuation || chr.isSymbol {
+                checks["isSymbol"] = true
             }
+            
         }
-        return start
-
-}
+        
+        if password != "" {
+            
+            if !checks["isLowercase"]! {
+                returnString += "Missing lowercased symbols\n"
+                score -= 1
+            }
+            if !checks["isUppercase"]! {
+                returnString += "Missing uppercased symbols\n"
+                score -= 1
+            }
+            if !checks["isSymbol"]! {
+                returnString += "Missing special symbols\n"
+                score -= 1
+            }
+            if !checks["isNumber"]! {
+                returnString += "Missing numbers\n"
+                score -= 1
+            }
+            if !checks["isLetter"]! {
+                returnString += "Missing letters\n"
+                score -= 1
+            }
+            
+            returnString += "\nPassword Score: \(score)"
+            
+        } else {
+            return "Please enter password"
+        }
+        
+        return returnString
+    }
+    
     
     
 }
