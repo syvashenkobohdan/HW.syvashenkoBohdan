5393def1e4862508227ec88932fdf8ef563b6250
SyvashenkoBohdan <syvashenkobohdan@icloud.com>
2020-10-27 11:32:08 +0200

    Assignment #3. задача 8

--- HW4/ViewController.swift
+++ HW4/ViewController.swift
@@ -11,37 +11,14 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(sorter(array: [9, 2, 6, 1, 3, 5, 1, 2]))
+        print(transliterate(str: "ДоРОГА"))
     }
     
-    func sorter(array: [Int]) -> [Int] {
-        var sortedArray = array
-        var dublicateErased = [Int]()
-        
-        
-        for firstIndex in 0..<array.count {
-            let passes = (sortedArray.count - 1) - firstIndex
-            
-            
-            for secondIndex in 0..<passes {
-                let key = sortedArray[secondIndex]
-                
-                
-                if (key > sortedArray[secondIndex + 1]) {
-                    sortedArray.swapAt(secondIndex, secondIndex + 1)
-                }
-            }
-        }
-        
-        
-        for value in sortedArray {
-            if dublicateErased.contains(value) == false {
-                dublicateErased.append(value)
-            }
-        }
-        
-        return dublicateErased
-        
+    func transliterate(str: String) -> String {
+        return str
+                    .applyingTransform(.toLatin, reverse: false)?
+                    .applyingTransform(.stripDiacritics, reverse: false) ?? str
+                    
     }
     
     
